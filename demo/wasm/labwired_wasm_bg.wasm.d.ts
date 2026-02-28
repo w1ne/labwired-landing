67e2496793f2d2a1f7b3d54560df75079b071067
w1ne <14119286+w1ne@users.noreply.github.com>
2026-02-28 01:30:19 +0100

    docs: sync agent doc fixes

---
+++ demo/wasm/labwired_wasm_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmsimulator_free: (a: number, b: number) => void;
+export const wasmsimulator_get_disassembly: (a: number) => [number, number];
+export const wasmsimulator_get_led_state: (a: number) => number;
+export const wasmsimulator_get_pc: (a: number) => number;
+export const wasmsimulator_get_register: (a: number, b: number) => number;
+export const wasmsimulator_get_register_names: (a: number) => any;
+export const wasmsimulator_new: (a: number, b: number) => [number, number, number];
+export const wasmsimulator_read_memory: (a: number, b: number, c: number) => [number, number];
+export const wasmsimulator_step: (a: number, b: number) => [number, number];
+export const wasmsimulator_step_single: (a: number) => [number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
